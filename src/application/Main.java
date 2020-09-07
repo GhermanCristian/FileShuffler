50d3c3015a0418414049c9706665da5c8ecd7f7e
GhermanCristian <gherman_cristi10@yahoo.com>
2020-09-07 13:24:58 +0300

    - The files in the selected folder are now displayed in a list

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,31 +1,57 @@
 package application;
 	
 import java.io.File;
+import java.util.List;
+import java.util.Vector;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class Main extends Application {
 	private File selectedDirectory = null;
 	
-	private void displayDirectoryContent() {
+	private List<File> getFilesInDirectory() {
+		List<File> filesInDirectory = new Vector<File>();
 		if (selectedDirectory != null) {
-	    	File[] files = selectedDirectory.listFiles();
-	    	if (files != null) {
-	    		for (File crtFile : files) {
+	    	File[] allFiles = selectedDirectory.listFiles();
+	    	if (allFiles != null) {
+	    		for (File crtFile : allFiles) { // make a lambda for this
 	    			if (crtFile.isFile() == true) {
-	    				System.out.println(crtFile);
+	    				filesInDirectory.add(crtFile);
 	    			}
 	    		}
 	    	}
 	    }
+		return filesInDirectory;
+	}
+	
+	private void clickedButtonAction(VBox mainProgramLayout, ListView<File> listView) {
+		Stage directoryChooserStage = new Stage();
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        
+		ObservableList<File> listItems;
+        
+        directoryChooser.setTitle("Choose directory");
+        directoryChooserStage.setAlwaysOnTop(true);
+        selectedDirectory = directoryChooser.showDialog(directoryChooserStage);
+        directoryChooserStage.setAlwaysOnTop(false);
+        
+        listItems = FXCollections.observableList(getFilesInDirectory());
+		listView.setItems(listItems);
+		mainProgramLayout.getChildren().add(listView);
 	}
 	
 	@Override
@@ -34,24 +60,25 @@ public class Main extends Application {
 		Button selectFolderButton;
 		Scene directoryChooserScene;
 		
+		VBox mainProgramLayout;
+		Scene mainScene;
+		
+		ListView<File> listView = new ListView<File>();
+		listView.setOrientation(Orientation.VERTICAL); // the orientation would've been vertical by default, but it's good to be safe
+		listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		
+		mainProgramLayout = new VBox(20); // gap = 20px
+		mainScene = new Scene(mainProgramLayout, 400, 400);
+		
 		directoryChooserLayout = new StackPane();
 		selectFolderButton = new Button("Choose folder");
 		selectFolderButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                Stage directoryChooserStage = new Stage();
-                DirectoryChooser directoryChooser = new DirectoryChooser();
-                directoryChooser.setTitle("Choose directory");
-                
-                primaryStage.setAlwaysOnTop(false);
-                directoryChooserStage.setAlwaysOnTop(true);
-                
-                selectedDirectory = directoryChooser.showDialog(directoryChooserStage);
-                displayDirectoryContent();
-                
+            	primaryStage.setAlwaysOnTop(false); // the setAlwaysOnTop stuff might not be that useful, but we keep it for now
+                clickedButtonAction(mainProgramLayout, listView);
                 primaryStage.setAlwaysOnTop(true);
-                directoryChooserStage.setAlwaysOnTop(false);
-                directoryChooserLayout.getChildren().remove(selectFolderButton); // I should set a new scene
+                primaryStage.setScene(mainScene); // after we click the button, we move to the next scene
             }
         });
 		
